bc0ba4dd9c910e4caebb5abdf78fd21e14422dfe
Willy Aguirre <marti1125@gmail.com>
2013-12-31 17:01:36 -0500

    save image

--- js/app.js
+++ js/app.js
@@ -1,15 +1,42 @@
+function randomUUID() {
+  var s = [], itoh = '0123456789ABCDEF';
+  for (var i = 0; i <36; i++) s[i] = Math.floor(Math.random()*0x10);
+  s[14] = 4;
+  s[19] = (s[19] & 0x3) | 0x8;
+  for (var i = 0; i <36; i++) s[i] = itoh[s[i]];
+  s[8] = s[13] = s[18] = s[23] = '_';
+  return s.join('');
+}
+
 document.addEventListener('DOMComponentsLoaded', function(){
 
   var pickImage = document.getElementById("pick-image");
-  var gaussianBlur = document.getElementById("gaussianBlur");
-  var horizontalBlur = document.getElementById("horizontalBlur");
-  var invertColor = document.getElementById("invertColor");
+  var filters = document.getElementById("filters");
+  var matrixInvert = document.getElementById("matrixInvert");
+  var save = document.getElementById("save");
   var img = document.querySelector("#image-presenter");
+  var flipBox = document.getElementById("flipBox");  
 
   var notificationPickImage = navigator.mozNotification.createNotification(
-                "Image",
+                "Pick Image",
                 "Please select an image"
             );
+
+  var notificationOnSuccess = navigator.mozNotification.createNotification(
+                "Success",
+                "The image has saved successfully"
+            );
+
+        var notificationOnError = navigator.mozNotification.createNotification(
+                "Error",
+                "Can not save the image"
+            );
+
+  filters.addEventListener("click", function(e){
+    if(img.getAttribute("src") != ""){
+      flipBox.toggle(); 
+    } 
+  });
 
   pickImage.addEventListener("click", function(e){
       var pick = new MozActivity({
@@ -28,8 +55,41 @@ document.addEventListener('DOMComponentsLoaded', function(){
       };
   });    
 
-  invertColor.addEventListener("click", function(e){
-      img.setAttribute("class", "i");
+  save.addEventListener("click", function(e){ 
+      var randomImage = randomUUID();
+
+      var cnv = document.getElementById("imageToSave");
+
+      var ctx=cnv.getContext("2d");
+      var imgToAdd=document.getElementById("image-presenter");
+      ctx.drawImage(imgToAdd,420,380);
+      //cnv.setAttribute("class", "i");
+      cnv.toBlob(function (blob) { 
+        var pic = navigator.getDeviceStorage("pictures");
+              
+        saveAndSend(blob);            
+
+        function saveAndSend(blob) { 
+          var storage = navigator.getDeviceStorage("pictures");
+          var request = storage.add(blob, "image-"+randomImage+".png");
+
+          request.onsuccess = function () {
+            notificationOnSuccess.show();                                                                                
+          }
+              
+          request.onerror = function () {
+            notificationOnError.show();                                        
+          }            
+        }                                         
+    
+      });
+
   });
+
+  // Filters
+  matrixInvert.addEventListener("click", function(e){
+    img.setAttribute("class", "i");
+    flipBox.toggle();
+  });  
 
 });
