1233c68c9525210fe5c9f42aff77c8e45d6feb7e
Willy Aguirre <marti1125@gmail.com>
2013-12-31 00:18:32 -0500

    update project delete svg.filter.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMComponentsLoaded', function(){
     var pickImage = document.getElementById("pick-image");
     var gaussianBlur = document.getElementById("gaussianBlur");
     var horizontalBlur = document.getElementById("horizontalBlur");
+    var invertColor = document.getElementById("invertColor");
     var img = document.querySelector("#image-presenter");
 
     pickImage.addEventListener("click", function(e){
@@ -20,22 +21,10 @@ document.addEventListener('DOMComponentsLoaded', function(){
         pick.onerror = function () {
             alert("Can't view the image!");
         };
-    });
+    });    
 
-    gaussianBlur.addEventListener("click", function(e){
-    	var draw = SVG('canvas').size('100%', '100%')
-        var image = draw.image(img.src).size(300, 300)
-        image.filter(function(add) {
-            add.gaussianBlur('30')
-        })
-    });
-
-    horizontalBlur.addEventListener("click", function(e){        
-        var draw = SVG('canvas').size('100%', '100%')
-        var image = draw.image(img.src).size(300, 300)
-        image.filter(function(add) {
-            add.gaussianBlur('30 0')
-        })
+    invertColor.addEventListener("click", function(e){
+        img.setAttribute("class", "i");
     });
 
 });
