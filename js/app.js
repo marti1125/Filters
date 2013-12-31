b70cc903d2ab54fe268b00a20dd8c7ca682a4ac9
Willy Aguirre <marti1125@gmail.com>
2013-12-31 01:20:08 -0500

    update bick and structure

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMComponentsLoaded', function(){
 
-    var pickImage = document.getElementById("pick-image");
-    var gaussianBlur = document.getElementById("gaussianBlur");
-    var horizontalBlur = document.getElementById("horizontalBlur");
-    var invertColor = document.getElementById("invertColor");
-    var img = document.querySelector("#image-presenter");
+  var pickImage = document.getElementById("pick-image");
+  var gaussianBlur = document.getElementById("gaussianBlur");
+  var horizontalBlur = document.getElementById("horizontalBlur");
+  var invertColor = document.getElementById("invertColor");
+  var img = document.querySelector("#image-presenter");
 
-    pickImage.addEventListener("click", function(e){
-        var pick = new MozActivity({
-            name: "pick",
-            data: {
-                type: ["image/png", "image/jpg", "image/jpeg"]
-            }
-        });
+  pickImage.addEventListener("click", function(e){
+      var pick = new MozActivity({
+          name: "pick",
+          data: {
+              type: ["image/png", "image/jpg", "image/jpeg"]
+          }
+      });
 
-        pick.onsuccess = function () {            
-            img.src = window.URL.createObjectURL(this.result.blob);            
-        };
+      pick.onsuccess = function () {            
+          img.src = window.URL.createObjectURL(this.result.blob);            
+      };
 
-        pick.onerror = function () {
-            alert("Can't view the image!");
-        };
-    });    
+      pick.onerror = function () {
+          alert("Can't view the image!");
+      };
+  });    
 
-    invertColor.addEventListener("click", function(e){
-        img.setAttribute("class", "i");
-    });
+  invertColor.addEventListener("click", function(e){
+      img.setAttribute("class", "i");
+  });
 
 });
